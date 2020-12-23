e6a32f5dacd8085077a3de27ff82be851fb04495
Gil Solis <gilsolisuw@gmail.com>
2020-12-22 21:01:24 -0800

    html mock up

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
 
+
+document.querySelector('#toggle').addEventListener('click', myFunction)
+
+function myFunction() {
+    var x = document.getElementById("horoscope");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
