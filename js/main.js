5de64e8174d9085a2fff74676e741e7fbed83256
Gil Solis <gilsolisuw@gmail.com>
2021-01-03 19:29:45 -0800

    updated toggle and tweaked date picker

--- js/main.js
+++ js/main.js
@@ -1,12 +1,38 @@
 
 
-document.querySelector('#toggle').addEventListener('click', myFunction)
+document.getElementById('toggle').addEventListener('click', toggleHoro)
 
-function myFunction() {
-    var x = document.getElementById("horoscope");
+document.getElementById("dateInput").addEventListener("change", birthday);
+
+document.getElementById("horoscopeBtn").addEventListener('click', decideYourFate)
+
+
+let dayOfBirth;
+let dateEntered = new Date(dayOfBirth);
+function decideYourFate() {
+    ;
+
+}
+
+function birthday() {
+    dayOfBirth = this.value;
+    let dateEntered = new Date(dayOfBirth);
+    month = dateEntered.getMonth()
+    day = dateEntered.getDate()
+    console.log(dayOfBirth);
+    console.log(month);
+    console.log(day)
+
+
+}
+
+function toggleHoro() {
+
+    let x = document.getElementById("horoscope");
     if (x.style.display === "none") {
         x.style.display = "block";
     } else {
         x.style.display = "none";
     }
 }
+
